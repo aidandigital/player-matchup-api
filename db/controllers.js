7e46dd4b1e13c72c8c0586fcc31fa4fd92b9f2bf
Aidan <84754533+aidan10371@users.noreply.github.com>
2022-09-30 22:42:21 -0400

    project less fields

--- db/controllers.js
+++ db/controllers.js
@@ -1,9 +1,17 @@
 const db = require("./db");
 
 module.exports = userController = {
-  getUsers: () => db.User.find({})
-    .sort({ creationDate: -1 })
-    .limit(150), // only show (or compare) the newest 150 users
+  getUsers: () => db.User.aggregate([
+    {
+      $sort: { creationDate: -1 }
+    },
+    {
+      $limit: 150 // only show (or compare) the newest 150 users
+    },
+    {
+      $project: {name: 1, xbox: 1, ps: 1, note: 1, answers: 1} // only show these non-sensitive fields
+    }
+  ]),
 
   checkEmailAvailability: async (email) => {
     const existingUser = await db.User.findOne({ email: email });
