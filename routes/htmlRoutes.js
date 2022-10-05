2c12d0a38a6d9c7ebc7aa87671da4349c49b92e7
Aidan <84754533+aidan10371@users.noreply.github.com>
2022-10-04 20:15:32 -0400

    html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,8 +1,5 @@
 const userController = require("../db/controllers");
 
-// DEPENDENCIES
-const path = require("path");
-
 module.exports = function(app) {
   app.get("/users", async (req, res) => {
     const users = await userController.getUsers();
