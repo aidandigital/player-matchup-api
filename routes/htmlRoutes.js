0008162ed1d6b9fed9c4b3388c14cd48ce3a7ec6
Aidan <84754533+aidan10371@users.noreply.github.com>
2022-10-04 20:12:20 -0400

    html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,17 +4,8 @@ const userController = require("../db/controllers");
 const path = require("path");
 
 module.exports = function(app) {
-  app.get("/survey", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/html/survey.html"));
-  });
-
   app.get("/users", async (req, res) => {
     const users = await userController.getUsers();
     res.json(users);
-  })
-
-  // Anything else goes to the home page:
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/html/home.html"))
   });
 };
